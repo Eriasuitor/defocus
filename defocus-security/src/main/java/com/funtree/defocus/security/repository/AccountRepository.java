c2e48e75780bef4dd7b7530863b98ab0c88bdddb
tooeztech_lory_jiang <funtree9@icloud.com>
2020-08-11 20:30:44 +0800

    init

--- defocus-security/src/main/java/com/funtree/defocus/security/repository/AccountRepository.java
+++ defocus-security/src/main/java/com/funtree/defocus/security/repository/AccountRepository.java
@@ -4,5 +4,5 @@ import com.funtree.defocus.security.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-    public Account findByUsername(String username);
+    Account findByUsername(String username);
 }
